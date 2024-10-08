ae0d3edadabf1d474d608b3e05d175f8d0128b96
sanjantist <123636995+sanjantist@users.noreply.github.com>
2024-10-08 16:45:14 +0300

    ebook

--- ebook.cpp
+++ ebook.cpp
@@ -9,67 +9,68 @@ namespace view = std::ranges::views;
 
 class Ebook {
    public:
-    void Solve(std::istream& is, std::ostream& os) {
-        int n;
-        is >> n;
-        os << std::setprecision(6);
-        for (int _ : view::iota(0, n)) {
-            std::string command;
-            is >> command;
-            if (command == "READ") {
-                Read(is);
-            } else if (command == "CHEER") {
-                CheerReader(is, os);
-            }
-        }
-    }
-
-   private:
-    std::vector<int> user_to_pages_read;
-    std::vector<int> page_to_achieved_users_;
-    int total_users_ = 0;
-
-    void CheerReader(std::istream& is, std::ostream& os) {
-        int user_id;
-        is >> user_id;
-        if ((int)user_to_pages_read.size() < user_id) {
+    double CheerReader(size_t user_id) {
+        if (user_to_pages_read.size() < user_id) {
             user_to_pages_read.resize(user_id * 2);
         }
         int read_pages = user_to_pages_read[user_id - 1];
         if (read_pages) {
             if (total_users_ - 1) {
-                os << (total_users_ - page_to_achieved_users_[read_pages - 1]) *
-                          1.0 / (total_users_ - 1)
-                   << "\n";
+                return (total_users_ -
+                        page_to_achieved_users_[read_pages - 1]) *
+                       1.0 / (total_users_ - 1);
             } else {
-                os << "1" << "\n";
+                return 1.0;
             }
         } else {
-            os << "0" << "\n";
+            return 0.0;
         }
     }
 
-    void Read(std::istream& is) {
-        int user_id, read_pages;
-        is >> user_id >> read_pages;
-        if ((int)user_to_pages_read.size() < user_id) {
+    void Read(size_t user_id, size_t read_pages) {
+        if (user_to_pages_read.size() < user_id) {
             user_to_pages_read.resize(user_id * 2);
         }
-        if ((int)page_to_achieved_users_.size() < read_pages) {
+        if (page_to_achieved_users_.size() < read_pages) {
             page_to_achieved_users_.resize(read_pages * 2);
         }
 
         if (!user_to_pages_read[user_id - 1]) {
             ++total_users_;
         }
-        for (int i : view::iota(user_to_pages_read[user_id - 1], read_pages)) {
+        for (size_t i :
+             view::iota(user_to_pages_read[user_id - 1], read_pages)) {
             ++page_to_achieved_users_[i];
         }
         user_to_pages_read[user_id - 1] = read_pages;
     }
+
+   private:
+    std::vector<size_t> user_to_pages_read;
+    std::vector<size_t> page_to_achieved_users_;
+    size_t total_users_ = 0;
 };
+
+void ParseRequests(std::istream& is, std::ostream& os, Ebook& ebook) {
+    int n;
+    is >> n;
+    os << std::setprecision(6);
+    for (int _ : view::iota(0, n)) {
+        std::string command;
+        is >> command;
+        if (command == "READ") {
+            size_t user_id, page;
+            is >> user_id >> page;
+            ebook.Read(user_id, page);
+        } else if (command == "CHEER") {
+            size_t user_id;
+            is >> user_id;
+            os << ebook.CheerReader(user_id) << "\n";
+        }
+    }
+}
 
 int main() {
     Ebook ebook;
-    ebook.Solve(std::cin, std::cout);
+    ParseRequests(std::cin, std::cout, ebook);
 }
